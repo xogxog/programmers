bde6d2900d830713849d0b4a737d03288ab21052
김태현 <nasa0939@naver.com>
2021-12-09 10:00:59 +0900

    update solved problems

--- 05_DFS_BFS/03_단어변환.py
+++ 05_DFS_BFS/03_단어변환.py
@@ -1,4 +1,38 @@
-def solution(begin, target, words):
+global_words =[]
+global_target =''
+global_answer = 987654321
 
-    answer = 0
-    return answer
+def dfs(tmp_cnt, visited, begin) :
+    global global_words, global_target, global_answer
+    if begin == global_target :
+        if tmp_cnt < global_answer :
+            global_answer = tmp_cnt
+        return
+
+    for i in range(len(global_words)) :
+        if not visited[i] and begin != global_words[i] :
+            cnt =0
+            for j in range(len(global_words[i])) : # 단어 정확히 하나 다른 것만 찾기
+                if begin[j] != global_words[i][j] :
+                    cnt +=1
+                if cnt >= 2:
+                    break
+            if cnt == 1 :
+                visited[i] = 1
+                dfs(tmp_cnt+1,visited, global_words[i])
+                visited[i] = 0
+            else :
+                continue
+
+def solution(begin, target, words):
+    global global_words, global_target, global_answer
+    global_words, global_target = words, target
+    visited = [0] * len(words)
+    if target not in words :
+         return 0
+    else :
+        dfs(0,visited, begin)
+
+    return global_answer
+
+# print(solution("hit","cog",["hot", "dot", "dog", "lot", "log", "cog"]))
--- 05_DFS_BFS/04_여행경로.py
+++ 05_DFS_BFS/04_여행경로.py
@@ -1,22 +1,49 @@
+global_tickets =[]
+answer =[]
+flag =0
+def dfs(cnt,visited) :
+    global answer, flag
+
+    if sum(visited) == len(visited) :
+        flag = 1
+        return answer
+    for i in range(len(global_tickets)) :
+        if not visited[i] and answer[-1] == global_tickets[i][0] :
+            # print(visited,answer)
+            visited[i] = 1
+            answer.append(global_tickets[i][1])
+            dfs(cnt+1,visited)
+            visited[i] = 0
+
+            if flag :
+                return
+            answer.pop()
+
+
+
+
+
+
+
 def solution(tickets):
+    global global_tickets, answer
+    tickets.sort(key=lambda x : x[1]) # 알파벳 빠른순으로 가기 위해
+    print(tickets)
+    global_tickets = tickets
     visited = [0] * len(tickets)
-    tmp =[]
-    answer = ["ICN"]
-    idx = 0
-    # Z의 아스키 코드
+    for i in range(len(tickets)) :
+        if tickets[i][0] == 'ICN' : # 출발지가 인천이면
+            visited[i] = 1
+            answer = tickets[i]
+            dfs(1,visited)
+            visited[i] = 0
+            if len(answer) == len(tickets)+1 :
+                return answer
 
-    while sum(visited) != len(visited) :
-        min_ord = 90
-        for i in range(len(tickets)) :
-            if not visited[i] and tickets[i][0] == answer[-1] :
-                if ord(tickets[i][1][0]) < min_ord :
-                    min_ord = ord(tickets[i][1][0])
-                    idx = i
-        visited[idx] = 1
-        answer.append(tickets[idx][1])
-
-
-    return answer
 
 # print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]))
-print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]))
+# print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]))
+# print(solution([["ICN", "B"], ["B", "ICN"], ["ICN", "A"], ["A", "D"], ["D", "A"]]))
+# print(solution([['ICN','AAA'],['ICN','AAA'],['ICN','AAA'],['AAA','ICN'],['AAA','ICN']]))
+print(solution([["ICN", "AOO"], ["AOO", "BOO"], ["BOO", "COO"], ["COO", "DOO"], ["DOO", "EOO"], ["EOO", "DOO"], ["DOO", "COO"], ["COO", "BOO"], ["BOO", "AOO"]]))
+# tickets=[["ICN", "B"], ["B", "ICN"], ["ICN", "A"], ["A", "D"], ["D", "A"]] # ["ICN", "B", "ICN", "A", "D", "A"]
